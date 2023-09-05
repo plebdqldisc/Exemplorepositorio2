04e965dc8b771a32577f3458cf2ad2a6dca9efc3
ARTHUR ANTONIO DE ARAUJO <arthur.7923@aluno.pr.senac.br>
2023-09-05 15:45:18 -0300

    primeiro commit

---
+++ src/main/java/com/mycompany/exemplorepositorio2/Exemplorepositorio2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exemplorepositorio2;
+
+/**
+ *
+ * @author arthur.7923
+ */
+public class Exemplorepositorio2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
