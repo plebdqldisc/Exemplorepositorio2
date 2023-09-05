57bcf31f102735684c9e6719c81e340d0a22fe4a
ARTHUR ANTONIO DE ARAUJO <arthur.7923@aluno.pr.senac.br>
2023-09-05 16:23:21 -0300

    primeira alteração

--- src/main/java/com/mycompany/exemplorepositorio2/Exemplorepositorio2.java
+++ src/main/java/com/mycompany/exemplorepositorio2/Exemplorepositorio2.java
@@ -11,6 +11,6 @@ package com.mycompany.exemplorepositorio2;
 public class Exemplorepositorio2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("feito");
     }
 }
